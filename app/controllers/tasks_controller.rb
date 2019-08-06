7371298b88b21cb8d4650c50cdd34175d06cc25b
Cihad Saglam <saglam.chd@gmail.com>
2019-08-06 17:19:51 +0200

    refactoring and DRy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
   def show
     # raise
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -21,17 +21,14 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_strong_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
@@ -40,5 +37,9 @@ class TasksController < ApplicationController
 
   def task_strong_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
